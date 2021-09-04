f4561b95e717689dc3102222ea564704f01ac29a
kudan0 <7xxx.ccs@gmail.com>
2021-09-04 16:02:27 +0000

    [add]name表示あたりまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'usere/index'
-  get 'usere/show'
-  get 'usere/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  root to: 'homes#top'
+  get 'homes/about'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
